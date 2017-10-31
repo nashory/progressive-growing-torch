d446d431643e71d48ca4102d803a3dbe9348b933
nashory <min.stellastra@gmail.com>
2017-10-31 21:22:23 +0900

    remove irrelevant files

---
+++ models/network.lua
@@ -0,0 +1,24 @@
+-- script for dealing with network
+-- resl: resolution-level (log2 scale)
+
+
+
+-- grow network
+function grow_network(gen, dis, resl)
+    print('gg')
+end
+
+
+-- return discriminator network.
+function get_discriminator()
+    print('discrim')
+end
+
+-- return generator network.
+function get_generator()
+    print('generator')
+end
+
+
+
+
